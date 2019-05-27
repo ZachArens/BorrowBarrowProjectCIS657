93119318dcaa832a1141a143a7135d4b6d8d0915
[ZachArens] <[arens.zach@gmail.com]>
2019-05-27 18:13:37 -0400

    Add tool shed item model and struct

---
+++ BorrowBarrowProjectCIS657/TSItemModel.swift
@@ -0,0 +1,30 @@
+//
+//  TSItemModel.swift
+//  BorrowBarrowProjectCIS657
+//
+//  Created by Zachary Arens on 5/27/19.
+//  Copyright © 2019 BarrelBox. All rights reserved.
+//
+
+import Foundation
+
+class TSItemModel {
+    fileprivate var items : [ToolShedItem] = [ToolShedItem]()
+    
+    init() {
+        createTSItems()
+    }
+    
+    func createTSItems() -> [ToolShedItem]
+    {
+        return self.items
+    }
+    
+    fileprivate func creatTSItems()
+    {
+        items.append(ToolShedItem(itemName: "Kayak", owner: "Barrel Box", itemDescription: "River kayak, 250 lb capacity, 10' long", reqYesNo: true, requirements: "Must provide transportation to/from water, responsible for fixing any damage or replacement if needed.", photo: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+        items.append(ToolShedItem(itemName: "Leaf Blower", owner: "Barrel Box", itemDescription: "gas powered backpack leaf blower", reqYesNo: true, requirements: "Must provide own gasoline, responsible for fixing any damage or replacement if needed.", photo: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+        items.append(ToolShedItem(itemName: "Cordless Drill", owner: "Barrel Box", itemDescription: "Lithium charging, charger provided", reqYesNo: true, requirements: "Must provide pick up/drop off, does not come with bits, responsible for fixing any damage or replacement if needed.", photo: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+        items.append(ToolShedItem(itemName: "Tile Saw", owner: "Barrel Box", itemDescription: "I will show you how to operate, 110v, up to 10\" tile", reqYesNo: true, requirements: "Requires water hookup, responsible for fixing any damage or replacement if needed.", photo: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+    }
+}
