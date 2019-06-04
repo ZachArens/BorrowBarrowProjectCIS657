8ccea2a2158341c809fc6108593f365eb5d80e69
Andy Vuong <vuonga@mail.gvsu.edu>
2019-06-04 13:47:50 -0400

    Implemented camera and photo library feature for AddItem and AddFriend views respectively

--- BorrowBarrowProjectCIS657/Controllers/AddFriendViewController.swift
+++ BorrowBarrowProjectCIS657/Controllers/AddFriendViewController.swift
@@ -8,31 +8,63 @@
 
 import UIKit
 
-class AddFriendViewController: UIViewController {
+class AddFriendViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var imgPickerCtrl: UIImagePickerController!;
+
+    @IBAction func cameraFriendBtn(_ sender: UIButton) {
+        
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+        {
+            imgPickerCtrl.sourceType = .camera
+            imgPickerCtrl.allowsEditing = true
+            imgPickerCtrl.delegate = self
+            present(imgPickerCtrl, animated: true);
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    @IBAction func photoLibFriendBtn(_ sender: UIButton) {
+        
+        imgPickerCtrl.sourceType = UIImagePickerController.SourceType.photoLibrary;
+        imgPickerCtrl.allowsEditing = true;
+        imgPickerCtrl.delegate = self;
+        present(imgPickerCtrl, animated: true);
+    }
+    
+    @IBOutlet weak var friendImageViewer: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imgPickerCtrl = UIImagePickerController();
 
         // Do any additional setup after loading the view.
     }
     
-//    @IBOutlet weak var friendImageView: UIImageView!
-//    
-//    @IBAction func addPictureFriendBtn(_ sender: Any) {
-//        
-//        let imgCtrl = UIImagePickerController();
-//        imgCtrl.delegate = self;
-//        imgCtrl.sourceType = .photoLibrary
-//        imgCtrl.allowsEditing = false;
-//        present(imgCtrl, animated: true, completion: nil);
-//    }
-//    
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo image: UIImage) {
-//        
-//        let chosenImg: UIImage = image;
-//        friendImageView.image = chosenImg;
-//        dismiss(animated: true, completion: nil);
-//    }
+    /*Image Picker Code  from sources:
+     
+     https://stackoverflow.com/questions/24625687/swift-uiimagepickercontroller-how-to-use-it
+     https://theswiftdev.com/2019/01/30/picking-images-with-uiimagepickercontroller-in-swift-5/
+     https://www.hackingwithswift.com/example-code/uikit/how-to-take-a-photo-using-the-camera-and-uiimagepickercontroller
+     
+     */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        let chosenImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        friendImageViewer.image = chosenImage;
+        dismiss(animated: true, completion: nil)
+        //Set Image View to image
+        
+    }
+
     
     /*
     // MARK: - Navigation
