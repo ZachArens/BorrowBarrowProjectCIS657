62307216e3734183880d731831a58230ade10058
[ZachArens] <[arens.zach@gmail.com]>
2019-06-07 21:08:24 -0400

    Added Community Firebase integration - still need to test.

--- BorrowBarrowProjectCIS657/Controllers/CommunityViewController.swift
+++ BorrowBarrowProjectCIS657/Controllers/CommunityViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 protocol CommunityDelegation{
     func usernameDelegate(username: String?)
@@ -23,6 +24,8 @@ class CommunityViewController: UIViewController, UITableViewDelegate, UITableVie
     
     var communityFriends : [CommunityFriend]?
     
+    fileprivate var ref : DatabaseReference?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
  
@@ -32,6 +35,9 @@ class CommunityViewController: UIViewController, UITableViewDelegate, UITableVie
         let model = ComItemModel()
         self.communityFriends = model.getComItems()
         self.setNeedsStatusBarAppearanceUpdate()
+        
+        self.ref = Database.database().reference()
+        self.registerForFireBaseUpdates()
     }
     
     
@@ -85,5 +91,58 @@ class CommunityViewController: UIViewController, UITableViewDelegate, UITableVie
             return cell
             
             
+    }
+    
+    fileprivate func registerForFireBaseUpdates()
+    {
+        self.ref!.child("community").observe(.value, with: { snapshot in
+            if let postDict = snapshot.value as? [String : AnyObject] {
+                var tmpItems = [CommunityFriend]()
+                for (_,val) in postDict.enumerated() {
+                    let key = val.1 as! Dictionary<String,AnyObject>
+                    let firstName = key["firstName"] as! String?
+                    let lastName = key["lastName"] as! String?
+                    let email = key["email"] as! String?
+                    let phoneNum = key["phoneNum"] as! String?
+                    let address1 = key["address1"] as! String?
+                    let address2 = key["address2"] as! String?
+                    let city = key["city"] as! String?
+                    let state = key["state"] as! String?
+                    let zipcode = key["zipcode"] as! String?
+                    let trustYesNo = key["trustYesNo"] as! Bool?
+                    let friendPhoto = key["friendPhoto"] as! String?
+                    
+                    
+                    tmpItems.append(CommunityFriend(firstName: firstName, lastName: lastName, email: email, phoneNum: phoneNum, address1: address1, address2: address2, city: city, state: state, zipcode: zipcode, trustYesNo: trustYesNo, friendPhoto: friendPhoto))
+                }
+                self.communityFriends = tmpItems
+            }
+        })
+        
+    }
+    
+    func toDictionary(itms: CommunityFriend) -> NSDictionary {
+        return [
+            "firstName": NSString(string: itms.firstName ?? ""),
+            "lastName": NSString(string: itms.lastName ?? ""),
+            "email": NSString(string: itms.email ?? ""),
+            "phoneNum": NSString(string: itms.phoneNum ?? ""),
+            "address1": NSString(string: itms.address1 ?? ""),
+            "address2": NSString(string: itms.address2 ?? ""),
+            "city": NSString(string: itms.city ?? ""),
+            "state": NSString(string: itms.state ?? ""),
+            "zipcode": NSString(string: itms.zipcode ?? ""),
+            "trustYesNo": Bool(booleanLiteral: itms.trustYesNo ?? false),
+            "friendPhoto": NSString(string: itms.friendPhoto ?? ""),
+            
+        ]
+    }
+    
+    func addItemToDB() {
+        // save history to firebase
+        //let addedItem = ToolShedItem(itemName: itemName, owner: owner, itemDescription: itemDescription, reqYesNo: reqYesNo, requirements: requirements, photo: photo, lentTo: lentTo))
+        let addedItem = CommunityFriend(firstName: "Darth", lastName: "Vader", email: "darth@celebrity.com", phoneNum: "555-555-5555", address1: "555 Broadway Ave.", address2: "", city: "New York", state: "NY", zipcode: "99999",trustYesNo: true,friendPhoto: "darth")
+        let newChild = self.ref?.child("community").childByAutoId()
+        newChild?.setValue(self.toDictionary(itms: addedItem))
     }
 }
--- BorrowBarrowProjectCIS657/Models/CommunityFriend.swift
+++ BorrowBarrowProjectCIS657/Models/CommunityFriend.swift
@@ -17,11 +17,13 @@ struct CommunityFriend {
     var address1: String?
     var address2: String?
     var city: String?
+    var state: String?
     var zipcode: String?
     var trustYesNo: Bool?
+    var friendPhoto: String?
 
     
-    init(key: String?, firstName: String?, lastName: String?, email: String?, phoneNum: String?, address1: String?, address2: String?, city: String?, zipcode: String?, trustYesNo: Bool?)
+    init(key: String?, firstName: String?, lastName: String?, email: String?, phoneNum: String?, address1: String?, address2: String?, city: String?, state: String?, zipcode: String?, trustYesNo: Bool?, friendPhoto: String?)
     {
         self.key = key;
         self.firstName = firstName;
@@ -31,17 +33,19 @@ struct CommunityFriend {
         self.address1 = address1;
         self.address2 = address2;
         self.city = city;
+        self.state = state;
         self.zipcode = zipcode;
         self.trustYesNo = trustYesNo;
+        self.friendPhoto = friendPhoto;
     }
     
-    init(firstName: String?, lastName: String?, email: String?, phoneNum: String?, address1: String?, address2: String?, city: String?, zipcode: String?, trustYesNo: Bool?)
+    init(firstName: String?, lastName: String?, email: String?, phoneNum: String?, address1: String?, address2: String?, city: String?, state: String?, zipcode: String?, trustYesNo: Bool?, friendPhoto: String?)
     {
-        self.init(key: nil, firstName: firstName, lastName: lastName, email: email, phoneNum: phoneNum, address1: address1, address2: address2, city: city, zipcode: zipcode, trustYesNo: trustYesNo)
+        self.init(key: nil, firstName: firstName, lastName: lastName, email: email, phoneNum: phoneNum, address1: address1, address2: address2, city: city, state: state, zipcode: zipcode, trustYesNo: trustYesNo, friendPhoto: friendPhoto)
     }
     
     init() {
-        self.init(key: nil, firstName: nil, lastName: nil, email: nil, phoneNum: nil, address1: nil, address2: nil, city: nil, zipcode: nil, trustYesNo: nil);
+        self.init(key: nil, firstName: nil, lastName: nil, email: nil, phoneNum: nil, address1: nil, address2: nil, city: nil, state: nil, zipcode: nil, trustYesNo: nil, friendPhoto: nil);
     }
     
 }
